19743bff94d964a69d712088f8aa3eb9a9a44eaf
ahmedullahmern <ahmedullahmern@gmail.com>
2024-10-02 02:49:22 +0500

    work done images issue

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,19 @@ import Cards from './componenets/Cards'
 function App() {
   const [products, setProducts] = useState([])
   const [cartItems, setCartItems] = useState([])
+  const [showCardItems, setshowCardItems] = useState(false)
 
   useEffect(() => {
     fetchProducts()
   }, [])
 
-  // useEffect(() => {
-  //   const localSeGetItems = JSON.parse(localStorage.getItem("cart"))
-  //   if (localSeGetItems) {
-  //     setCartItems(...localSeGetItems)
-  //   }
-  // },[])
+  useEffect(() => {
+    const localSeGetItems = JSON.parse(localStorage.getItem("cart"))
+    console.log("local==>", localSeGetItems)
+    if (localSeGetItems) {
+      setCartItems([...localSeGetItems])
+    }
+  }, [])
 
   useEffect(() => {
     if (cartItems.length) {
@@ -43,32 +45,47 @@ function App() {
     const products = await getAllproducts();
     setProducts([...products])
     console.log("products", products);
-
   }
+
+  const iterateOn = showCardItems ? cartItems : products
 
   return (
     <>
       <div className='container mx-auto my-10'>
-        <div className='fixed w-full h-[100px] bg-black text-white top-0  flex items-center justify-center gap-10'>
-          <h1 className='text-center text-1xl font-semibold'>Shopping List</h1>
-          <h1 className='text-center text-1xl underline'>Card items : {cartItems.length}</h1>
+        <div className='fixed w-full h-[80px] bg-black text-white top-0  flex items-center justify-center gap-10'>
+          <h1 className='text-center text-2xl font-semibold'>Shopping List</h1>
+          <h1
+            onClick={() => setshowCardItems(!showCardItems)}
+            className='text-center text-2xl underline cursor-pointer hover:text-blue-500'
+          >
+            {
+              showCardItems ? "Show All Products" :
+                `Card items : ${cartItems.length}`
+            }
+          </h1>
         </div>
         <section className="text-gray-600 body-font">
           <div className="container px-5 py-24 mx-auto">
             <div className="flex flex-wrap -m-4">
-              {products.map((data, ind) => {
+              {/* {showCardItems} */}
+              {iterateOn.map((data, ind) => {
                 const isAddToCart = cartItems.findIndex((produtc) => produtc.id === data.id) !== -1
                 return (
                   <Cards
                     isAddToCart={isAddToCart}
                     addToCard={() => addCardItems(data)} key={data.id} khan={data}
+                    showRemoveFromCart={showCardItems === true}
+                    removeCards={() => {
+                      const allOtherProduts = cartItems.filter((products) => products.id !== data.id);
+                      setCartItems([...allOtherProduts])
+                    }}
                   />
                 )
               })}
             </div>
           </div>
         </section>
-      </div>
+      </div >
 
 
 
--- src/componenets/Cards.jsx
+++ src/componenets/Cards.jsx
@@ -1,13 +1,12 @@
-function Cards({ khan, addToCard, isAddToCart }) {
+function Cards({ khan, addToCard, isAddToCart, showRemoveFromCart, removeCards }) {
     const { category, images, description, price, title } = khan
-    console.log("Image URL:", images[0]);
 
     return (
         <div className="p-4 md:w-1/3">
             <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
                 <img
                     className="lg:h-48 md:h-36 w-full object-cover object-center"
-                    src={images}
+                    src={`${images}`}
                     alt="API Ka Issue He"
                 />
                 <div className="p-6">
@@ -38,39 +37,31 @@ function Cards({ khan, addToCard, isAddToCart }) {
                                 <path d="M12 5l7 7-7 7" />
                             </svg>
                         </a>
-                        <span className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                            <svg
-                                className="w-4 h-4 mr-1"
-                                stroke="currentColor"
-                                strokeWidth={2}
-                                fill="none"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                viewBox="0 0 24 24"
-                            >
-                                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
-                                <circle cx={12} cy={12} r={3} />
-                            </svg>
-                            {price}
-                        </span>
-                        <span className="text-gray-400 inline-flex items-center leading-none text-sm">
-                            <svg
-                                className="w-4 h-4 mr-1"
-                                stroke="currentColor"
-                                strokeWidth={2}
-                                fill="none"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                viewBox="0 0 24 24"
-                            >
-                                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
-                            </svg>
-                            6
-                        </span>
                     </div>
+                    {showRemoveFromCart && (
+                        <div
+                            onClick={removeCards}
+                            className="flex items-center flex-wrap cursor-pointer mt-3">
+                            < a className=" text-red-600 inline-flex items-center md:mb-2 lg:mb-0">
+                                {"Remove From Cart"}
+                                <svg
+                                    className="w-4 h-4 ml-2"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    strokeWidth={2}
+                                    fill="none"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                >
+                                    <path d="M5 12h14" />
+                                    <path d="M12 5l7 7-7 7" />
+                                </svg>
+                            </a>
+                        </div>
+                    )}
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
